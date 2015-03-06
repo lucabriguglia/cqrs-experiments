424ae880361c9d5b0e21180d5b2b5aa0cacbadd0
Luca Briguglia <luca.briguglia@outlook.com>
2015-03-06 16:21:13 +0000

    Added unit tests for blog and category

--- src/Weapsy.Blog.Domain.Tests/BlogTests.cs
+++ src/Weapsy.Blog.Domain.Tests/BlogTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using NUnit.Framework;
 using Weapsy.Blog.Domain.Blog.Events;
+using Weapsy.Blog.Domain.Blog.Exceptions;
 
 namespace Weapsy.Blog.Domain.Tests
 {
@@ -40,5 +41,51 @@ namespace Weapsy.Blog.Domain.Tests
 			Assert.AreEqual(blog.Id, @event.Id);
 			Assert.AreEqual(blog.Title, @event.Title);
 		}
+
+        [Test]
+        public void Should_throw_blog_already_deleted_exception_when_delete_deleted_blog()
+        {
+            var blog = Blog.Blog.CreateNew("My Blog");
+            blog.GetType().GetProperty("Deleted").SetValue(blog, true, null);
+
+            Assert.Throws<BlogAlreadyDeletedException>(() => blog.Delete());
+        }
+
+        [Test]
+        public void Should_delete_blog_and_notify()
+        {
+            var blog = Blog.Blog.CreateNew("My Blog");
+
+            blog.Delete();
+
+            var @event = blog.Events.OfType<BlogDeletedEvent>().SingleOrDefault();
+
+            Assert.IsTrue(blog.Deleted);
+            Assert.IsNotNull(@event);
+            Assert.AreEqual(blog.Id, @event.Id);
+        }
+
+        [Test]
+        public void Should_throw_blog_not_deleted_exception_when_restore_deleted_blog()
+        {
+            var blog = Blog.Blog.CreateNew("My Blog");
+
+            Assert.Throws<BlogNotDeletedException>(() => blog.Restore());
+        }
+
+        [Test]
+        public void Should_restore_blog_and_notify()
+        {
+            var blog = Blog.Blog.CreateNew("My Blog");
+            blog.GetType().GetProperty("Deleted").SetValue(blog, true, null);
+
+            blog.Restore();
+
+            var @event = blog.Events.OfType<BlogRestoredEvent>().SingleOrDefault();
+
+            Assert.IsFalse(blog.Deleted);
+            Assert.IsNotNull(@event);
+            Assert.AreEqual(blog.Id, @event.Id);
+        }
 	}
 }
--- src/Weapsy.Blog.Domain.Tests/CategoryTests.cs
+++ src/Weapsy.Blog.Domain.Tests/CategoryTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using NUnit.Framework;
 using Weapsy.Blog.Domain.Category.Events;
+using Weapsy.Blog.Domain.Category.Exceptions;
 
 namespace Weapsy.Blog.Domain.Tests
 {
@@ -43,5 +44,51 @@ namespace Weapsy.Blog.Domain.Tests
 			Assert.AreEqual(category.Id, @event.Id);
 			Assert.AreEqual(category.Title, @event.Title);
 		}
+
+        [Test]
+        public void Should_throw_category_already_deleted_exception_when_delete_deleted_category()
+        {
+            var category = Category.Category.CreateNew(Guid.NewGuid(), "My category");
+            category.GetType().GetProperty("Deleted").SetValue(category, true, null);
+
+            Assert.Throws<CategoryAlreadyDeletedException>(() => category.Delete());
+        }
+
+        [Test]
+        public void Should_delete_category_and_notify()
+        {
+            var category = Category.Category.CreateNew(Guid.NewGuid(), "My category");
+
+            category.Delete();
+
+            var @event = category.Events.OfType<CategoryDeletedEvent>().SingleOrDefault();
+
+            Assert.IsTrue(category.Deleted);
+            Assert.IsNotNull(@event);
+            Assert.AreEqual(category.Id, @event.Id);
+        }
+
+        [Test]
+        public void Should_throw_category_not_deleted_exception_when_restore_deleted_category()
+        {
+            var category = Category.Category.CreateNew(Guid.NewGuid(), "My category");
+
+            Assert.Throws<CategoryNotDeletedException>(() => category.Restore());
+        }
+
+        [Test]
+        public void Should_restore_category_and_notify()
+        {
+            var category = Category.Category.CreateNew(Guid.NewGuid(), "My category");
+            category.GetType().GetProperty("Deleted").SetValue(category, true, null);
+
+            category.Restore();
+
+            var @event = category.Events.OfType<CategoryRestoredEvent>().SingleOrDefault();
+
+            Assert.IsFalse(category.Deleted);
+            Assert.IsNotNull(@event);
+            Assert.AreEqual(category.Id, @event.Id);
+        }
 	}
 }
